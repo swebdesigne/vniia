23312c9210aaaf232104e617ef2c56f0b131ab49
swebdesigne <swebdesigne@gmail.com>
2024-03-28 17:13:20 +0300

    Project init

---
+++ src/main/java/com/vniia/project/vniia/VniiaApplication.java
@@ -0,0 +1,13 @@
+package com.vniia.project.vniia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VniiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VniiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vniia/project/vniia/VniiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vniia.project.vniia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VniiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
